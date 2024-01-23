8a3332635e744f2eed3878f002e56a9269ce3e8b
mahesh pallapothula <108536569+Maheshpallapothula@users.noreply.github.com>
2024-01-23 18:12:39 +0530

    updated initial commit

--- server.js
+++ server.js
@@ -1,69 +1,77 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
 const app = express();
-require('dotenv/config');
-const bodyParser = require('body-parser');
-
-
-/* Intializing Cors */
-const cors = require('cors');
+const bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+const cors = require("cors");
 app.use(cors());
+require("dotenv/config");
+const jwt = require("jsonwebtoken");
 
-/* body-parser */
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
+const users = [
+  {
+    id: 1234,
+    userName: "mahesh",
+    password: "Passd@1234",
+  },
+];
 
+app.post("/login", (req, res) => {
+  const { userName = "", password = "" } = req.body;
 
-app.listen(process.env.port);
+  const user = users.find((user) => {
+    return user.userName === userName && user.password === password;
+  });
 
-//**configuring users for logging. */
-const users = [{
-    userName: 'mahesh',
-    password: "Passd@1234"
-}];
+  const accessToken = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "15m", // Access token expires in 15 minutes
+    }
+  );
 
+  const refreshToken = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "48h" }
+  );
+
+  res.status(200).json({ accessToken, refreshToken });
+});
 
 // Middleware to check if the request has a valid access token
 const authenticateToken = (req, res, next) => {
-    const token = req.header('Authorization');
-  
-    if (!token) return res.sendStatus(401);
-  
-    jwt.verify(token, secretKey, (err, user) => {
-      if (err) return res.sendStatus(403);
-      req.user = user;
-      next();
-    });
+  const token = req.header("Authorization");
+
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
 };
 
-// Login endpoint to issue initial access and refresh tokens
-app.post('/login', (req, res) => {
-    console.log("loginnnn");
-    const { userName, password } = req.body;
-  
-    const user = users.find((u) => u.userName === userName && u.password === password);
+app.post("/renewAccessToken", (req, res) => {
+  const refreshToken = req.headers["refreshtoken"];
+  console.log("🚀 ~ app.post ~ refreshToken:", refreshToken);
 
-    console.log("userbnsgfbfbfb", user);
-  
-    // if (!user) return res.status(401).send("Failure to get user's.");
-  
-    const accessToken = jwt.sign({ id: user.id, username: user.username }, process.env.ACCESS_TOKEN_SECRET, {
-      expiresIn: '15m', // Access token expires in 15 minutes
-    });
-  
-    const refreshToken = jwt.sign({ id: user.id, username: user.username }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '48h'});
-  
-    res.status(200).json({ accessToken, refreshToken });
+  if (!refreshToken) return res.status(401).send("Invalid refresh token.");
+
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.status(403).send("Invalid refresh token.");
+
+    const accessToken = jwt.sign(
+      { id: user.id, username: user.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "15m",
+      }
+    );
+
+    res.json({ accessToken });
+  });
 });
 
-
-app.get("", (res,req) => {
-    console.log("Hii Jwt is On Up.!");
-    
-})
-
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on http://localhost:${process.env.PORT}`);
-});
-
+app.listen(process.env.PORT || 3000);
